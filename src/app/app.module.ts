b083411011204ee54c8de23c432f93063bb68ba2
AndrewGithinji <andrewgithinji@hotmail.com>
2023-07-11 19:04:19 +0300

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoiceRecognitionComponent } from './voice-recognition/voice-recognition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoiceRecognitionComponent
   ],
   imports: [
     BrowserModule,
