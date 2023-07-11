43b63a68031b0b7207f791d31d8341f132c7579a
AndrewGithinji <andrewgithinji@hotmail.com>
2023-07-11 19:29:32 +0300

    voice recog component spec fix

--- src/app/voice-recognition/voice-recognition.component.spec.ts
+++ src/app/voice-recognition/voice-recognition.component.spec.ts
@@ -2,6 +2,38 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VoiceRecognitionComponent } from './voice-recognition.component';
 
+interface SpeechRecognitionEvent extends Event {
+  results: SpeechRecognitionResultList;
+}
+
+interface SpeechRecognitionResultList extends Array<SpeechRecognitionResult> {
+  item(index: number): SpeechRecognitionAlternative;
+}
+
+interface SpeechRecognitionResult {
+  [index: number]: SpeechRecognitionAlternative;
+  length: number;
+}
+
+interface SpeechRecognitionAlternative {
+  transcript: string;
+  confidence: number;
+}
+
+class MockSpeechRecognition extends EventTarget {
+  constructor() {
+    super();
+  }
+
+  public start() {
+    // Mock implementation
+  }
+
+  public stop() {
+    // Mock implementation
+  }
+}
+
 describe('VoiceRecognitionComponent', () => {
   let component: VoiceRecognitionComponent;
   let fixture: ComponentFixture<VoiceRecognitionComponent>;
@@ -27,14 +59,49 @@ describe('VoiceRecognitionComponent', () => {
   });
 
   it('should append p element to texts element when there is a recognized result', () => {
-    const mockResult = {
+    const mockResult: SpeechRecognitionEvent = {
       results: [
         {
-          0: { transcript: 'Hello' },
-          isFinal: true,
-        },
-      ],
-    } as SpeechRecognitionEvent;
+          item(index: number): SpeechRecognitionAlternative {
+            return this[index];
+          },
+          0: { transcript: 'Hello', confidence: 1 },
+          length: 1,
+        } as SpeechRecognitionResult,
+      ] as SpeechRecognitionResultList,
+      type: 'result',
+      target: new MockSpeechRecognition(),
+      bubbles: false,
+      cancelBubble: false,
+      cancelable: false,
+      composed: false,
+      currentTarget: null,
+      defaultPrevented: false,
+      eventPhase: 0,
+      isTrusted: false,
+      returnValue: false,
+      srcElement: null,
+      timeStamp: 0,
+      composedPath: function (): EventTarget[] {
+        throw new Error('Function not implemented.');
+      },
+      initEvent: function (type: string, bubbles?: boolean | undefined, cancelable?: boolean | undefined): void {
+        throw new Error('Function not implemented.');
+      },
+      preventDefault: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      stopImmediatePropagation: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      stopPropagation: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      NONE: 0,
+      CAPTURING_PHASE: 1,
+      AT_TARGET: 2,
+      BUBBLING_PHASE: 3
+    };
 
     spyOn(document, 'querySelector').and.returnValue({} as HTMLElement);
     component.recognition.dispatchEvent(new Event('result', mockResult));
@@ -45,14 +112,49 @@ describe('VoiceRecognitionComponent', () => {
   });
 
   it('should handle recognized phrases and generate responses correctly', () => {
-    const mockResult = {
+    const mockResult: SpeechRecognitionEvent = {
       results: [
         {
-          0: { transcript: 'How are You?' },
-          isFinal: true,
-        },
-      ],
-    } as SpeechRecognitionEvent;
+          item(index: number): SpeechRecognitionAlternative {
+            return this[index];
+          },
+          0: { transcript: 'How are you?', confidence: 1 },
+          length: 1,
+        } as SpeechRecognitionResult,
+      ] as SpeechRecognitionResultList,
+      type: 'result',
+      target: new MockSpeechRecognition(),
+      bubbles: false,
+      cancelBubble: false,
+      cancelable: false,
+      composed: false,
+      currentTarget: null,
+      defaultPrevented: false,
+      eventPhase: 0,
+      isTrusted: false,
+      returnValue: false,
+      srcElement: null,
+      timeStamp: 0,
+      composedPath: function (): EventTarget[] {
+        throw new Error('Function not implemented.');
+      },
+      initEvent: function (type: string, bubbles?: boolean | undefined, cancelable?: boolean | undefined): void {
+        throw new Error('Function not implemented.');
+      },
+      preventDefault: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      stopImmediatePropagation: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      stopPropagation: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      NONE: 0,
+      CAPTURING_PHASE: 1,
+      AT_TARGET: 2,
+      BUBBLING_PHASE: 3
+    };
 
     spyOn(document, 'querySelector').and.returnValue({} as HTMLElement);
     component.recognition.dispatchEvent(new Event('result', mockResult));
@@ -60,6 +162,6 @@ describe('VoiceRecognitionComponent', () => {
 
     const appendedPElement = component.texts.querySelector('.replay');
     expect(appendedPElement).toBeTruthy();
-    expect(appendedPElement?.innerText).toBe('I am fine');
+    expect(appendedPElement?.textContent).toBe('I am fine');
   });
 });
