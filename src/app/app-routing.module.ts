c06f00361a396d4fb2194eea8b6b096f17e7676d
AndrewGithinji <andrewgithinji@hotmail.com>
2023-07-12 17:51:59 +0300

    voice recog app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VoiceRecognitionComponent } from './voice-recognition/voice-recognition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: VoiceRecognitionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
